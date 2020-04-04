14a776e4b6384820bf2f28c4a7233a7527a1c126
Christian Passarelli <cpassare@redhat.com>
2020-04-05 01:31:21 +0200

    carico

--- index.php
+++ index.php
@@ -4,9 +4,20 @@ require("./env/config.php");
 
 // Check connection
 if ($conn->connect_error) {
+?>
+<h2 style="color:red">DB NOT CONNECTED</h2>
+<?php
     die("Connection failed: " . $conn->connect_error);
+
 }
-echo "Connected successfully";
+//echo "Connected successfully";
+?>
+
+<h2 style="color:green">DB connected</h2>
+
+
+<?php
+
 
 $conn->close();
 
